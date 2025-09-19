c6bad31073e9a2ae3dddb8976bbcf4aaf7b3e9b2
Salamatov Dmitry <dimasalam@yandex.ru>
2025-09-19 19:18:41 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-    base: "/ltch/dist",
-  plugins: [react()],
-})
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return defineConfig({
+        base: env.SITE_URL,
+        plugins: [react()],
+    });
+}
